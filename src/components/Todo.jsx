48d658aaade65dffb2c67ba03891c6949a7e5cd8
Kaichun <i1am2boss3@gmail.com>
2024-11-04 19:41:27 +0800

    add title

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -71,7 +71,7 @@ const Todo = () => {
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
       <div className="flex items-center mt-7 gap-2">
         <MdDateRange className="w-8 h-14" />
-        <h1 className="text-3xl font-semibold">To-Do List</h1>
+        <h1 className="text-3xl font-semibold">To-Do List (test)</h1>
         <span className="ml-2 text-gray-500 text-xl">
           {currentDate.toLocaleDateString()}
         </span>
